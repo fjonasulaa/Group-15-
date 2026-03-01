4b8120caa134938c3bd4a7e2cfd64ca6af9e1e22
Menahil Khan <240096641@acton.ac.uk>
2026-03-01 06:47:41 +0000

    navbar updates

--- frontend/html/account.php
+++ frontend/html/account.php
@@ -192,6 +192,7 @@ $user = $userQuery->fetch_assoc();
       <a href="wines.html">Wines</a>
       <a href="basket.php">Basket</a>
       <a href="contact-us.php">Contact Us</a>
+      <a href="websiteReviews.html">Reviews</a>
     </div>
 
     <div class="navbar-right">
