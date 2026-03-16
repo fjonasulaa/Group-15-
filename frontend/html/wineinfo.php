c3aa18bb2f1d64d28ec63fde8267fb7ef7ed9883
Sukhi771 <240093695@aston.ac.uk>
2026-03-16 22:49:50 +0000

    Update wineinfo.php

--- frontend/html/wineinfo.php
+++ frontend/html/wineinfo.php
@@ -205,13 +205,38 @@ $reviewJustSubmitted = isset($_GET['review']) && $_GET['review'] === "success";
 <div class="wrap-cards">
     <div class="info-card">
 
-        <div class="images">
-            <div class="front-image">
-                <img src="<?php echo htmlspecialchars($mainImage); ?>"
-                     alt="<?php echo htmlspecialchars($wine['wineName']); ?>"
-                     style="width:100%; border-radius:10px;">
+        <div class="product-imgs">
+    <div class="img-display">
+        <div class="image-showcase">
+            <img src="../../images/<?php echo htmlspecialchars($wine['imageUrl']); ?>" alt="wine 1">
+            <img src="../../images/<?php echo htmlspecialchars($wine['img2']); ?>" alt="wine 2">
+            <img src="../../images/<?php echo htmlspecialchars($wine['img3']); ?>" alt="wine 3">
+            <img src="../../images/<?php echo htmlspecialchars($wine['img4']); ?>" alt="wine 4">
+        </div>
+    </div>
+    <div class="select-image">
+        <div class="item">
+            <a href="#" data-id="1">
+                <img src="../../images/<?php echo htmlspecialchars($wine['imageUrl']); ?>" alt="wine">
+            </a>
+        </div>
+        <div class="item">
+            <a href="#" data-id="2">
+                <img src="../../images/<?php echo htmlspecialchars($wine['img2']); ?>" alt="wine">
+            </a>
+        </div>
+        <div class="item">
+            <a href="#" data-id="3">
+                <img src="../../images/<?php echo htmlspecialchars($wine['img3']); ?>" alt="wine">
+            </a>
+        </div>
+        <div class="item">
+            <a href="#" data-id="4">
+                <img src="../../images/<?php echo htmlspecialchars($wine['img4']); ?>" alt="wine">
+            </a>
             </div>
         </div>
+    </div>
 
         <div class="content">
             <h2 class="title"><?php echo htmlspecialchars($wine['wineName']); ?></h2>
@@ -824,6 +849,27 @@ function syncWishlistButton(list) {
 }
 
 loadWishlist();
+
+const imgs = document.querySelectorAll('.select-image a');
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.image-showcase img:first-child').clientWidth;
+    document.querySelector('.image-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+
+window.addEventListener('resize', slideImage);
+    
 </script>
 
 <footer class="footer">
